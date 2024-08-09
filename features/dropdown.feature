fbdb05daaf6578c7b88a8d73a1bd31c704907950
Micah Resnick <resnickmicah@gmail.com>
2024-08-08 22:55:06 -0400

    Improve console logging

--- features/dropdown.feature
+++ features/dropdown.feature
@@ -11,6 +11,8 @@ Feature: The Internet Guinea Pig Website
       | Option 1 |
       | Option 2 |
 
+  # Is there a preference to always use "Scenario Outline"?
+  # I think just "Scenario" would work here since we're not parametrizing
   Scenario Outline: The dropdown should have a placeholder
     Given I am on the Dropdown page
     Then The dropdown value should be "Please select an option"
--- features/step-definitions/dropdown.js
+++ features/step-definitions/dropdown.js
@@ -3,12 +3,15 @@ import { expect } from "@wdio/globals";
 import dropdownPage from "../pageobjects/dropdown.page.js";
 
 When(/^I select "(.+)"$/, async function (option) {
+  // not sure what you're going for here?
   world.hello = "hello";
-  console.log(world);
+  // This seems like more useful information about what properties you've already
+  // Assigned to world than the previous call of: console.log(world)
+  console.log(Object.entries(world));
   await dropdownPage.select(option);
 });
 
 Then(/^The dropdown value should be "(.+)"$/, async function (option) {
-  console.log(world);
+  console.log(Object.entries(world));
   expect(await dropdownPage.selectedOptionText()).toBe(option);
 });
