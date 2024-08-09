e66c881d384e32b2ffaf55d9639753f7ff6f3280
Micah Resnick <resnickmicah@gmail.com>
2024-08-08 20:18:26 -0400

    Fix checkboxes.feature

--- features/step-definitions/checkboxes.js
+++ features/step-definitions/checkboxes.js
@@ -1,12 +1,25 @@
-import { When, Then } from "@wdio/cucumber-framework";
+import { Given, When, Then } from "@wdio/cucumber-framework";
 import { expect } from "@wdio/globals";
 import checkboxesPage from "../pageobjects/checkboxes.page.js";
 
-When(/^I select checkbox (\d)$/, async function (num) {
+Given(/^I see that checkbox (\d) is (checked|unchecked)$/, async function (num, checkboxState) {
   this.checkbox = await checkboxesPage.elements.checkbox(num);
-  await checkboxesPage.select(num);
+
+  if (checkboxState === "checked") {
+    await expect(this.checkbox).toHaveAttribute("checked");
+  } else {
+    await expect(this.checkbox).not.toHaveAttribute("checked");
+  }
 });
 
-Then(/^The checkbox should be checked$/, async function () {
-  await expect(this.checkbox).toHaveAttribute("checked");
+When(/^I click the checkbox$/, async function () {
+  await this.checkbox.click();
+});
+
+Then(/^The checkbox should be (checked|unchecked)$/, async function (checkboxState) {
+  if (checkboxState === "checked") {
+    await expect(this.checkbox).toHaveAttribute("checked");
+  } else {
+    await expect(this.checkbox).not.toHaveAttribute("checked");
+  }
 });
