bd5be02992627e1d19ac281e989da157f29705a6
Micah Resnick <resnickmicah@gmail.com>
2024-08-08 22:38:03 -0400

    Change this to world
    https://github.com/cucumber/cucumber-js/blob/main/docs/support_files/world.md#arrow-functions

--- features/step-definitions/checkboxes.js
+++ features/step-definitions/checkboxes.js
@@ -1,25 +1,25 @@
-import { Given, When, Then } from "@wdio/cucumber-framework";
+import { Given, When, Then, world } from "@cucumber/cucumber";
 import { expect } from "@wdio/globals";
 import checkboxesPage from "../pageobjects/checkboxes.page.js";
 
 Given(/^I see that checkbox (\d) is (checked|unchecked)$/, async function (num, checkboxState) {
-  this.checkbox = await checkboxesPage.elements.checkbox(num);
+  world.checkbox = await checkboxesPage.elements.checkbox(num);
 
   if (checkboxState === "checked") {
-    await expect(this.checkbox).toHaveAttribute("checked");
+    await expect(world.checkbox).toHaveAttribute("checked");
   } else {
-    await expect(this.checkbox).not.toHaveAttribute("checked");
+    await expect(world.checkbox).not.toHaveAttribute("checked");
   }
 });
 
 When(/^I click the checkbox$/, async function () {
-  await this.checkbox.click();
+  await world.checkbox.click();
 });
 
 Then(/^The checkbox should be (checked|unchecked)$/, async function (checkboxState) {
   if (checkboxState === "checked") {
-    await expect(this.checkbox).toHaveAttribute("checked");
+    await expect(world.checkbox).toHaveAttribute("checked");
   } else {
-    await expect(this.checkbox).not.toHaveAttribute("checked");
+    await expect(world.checkbox).not.toHaveAttribute("checked");
   }
 });
